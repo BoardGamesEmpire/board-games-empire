e78034f6dbb7cd61290c69023390bc015498590b
jfrazx <staringblind@gmail.com>
2025-05-02 17:18:04 -0700

    feat(cleanup): remove non-conforming previously generated modules

--- apps/api/src/app/mechanics/dto/create-mechanic.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateMechanicDto {
-  @IsNotEmpty()
-  @IsString()
-  name: string;
-}
--- apps/api/src/app/publishers/dto/create-publisher.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class CreatePublisherDto {
-  @IsNotEmpty()
-  @IsString()
-  name: string;
-}
